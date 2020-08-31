9fbf7ba5fa277ce2eb9eb861f33a5eef138df0c9
Teva RIOU <teva.riou@gmail.com>
2020-08-31 02:58:27 +0200

    feat: multithreading

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hello Rust!
+# Hello, Rust!
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,20 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs;
+use std::{fs, thread};
+use std::time::Duration;
+use webserv::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(5) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -21,8 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(3));
         ("HTTP/1.1 200 OK\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
